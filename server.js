53e00070857310e01152e127fa9e699a72c31e63
ppal-00 <162554639+ppal-00@users.noreply.github.com>
2025-06-27 02:56:21 +0530

    fix: allow CORS for frontend

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 // Routes
 app.use('/api/chargers', require('./routes/chargers'));
