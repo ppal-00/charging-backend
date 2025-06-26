698fd138c0c131654e6401cbff034e1da0d1035d
ppal-00 <162554639+ppal-00@users.noreply.github.com>
2025-06-27 03:20:35 +0530

    fix: full CORS config with preflight 3

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:8080',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
